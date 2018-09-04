e2a9165251c0a96ad585a15e4bd6c6c88cce1de9
ConorHand0 <42778195+ConorHand0@users.noreply.github.com>
2018-09-04 18:25:24 +0100

    Add files via upload

--- Repeat_IoT_-_-.ino
+++ Repeat_IoT_-_-.ino
@@ -1,10 +1,16 @@
 const int ledPin =  6;
 int soundSensorPin = A0;
+#include<Bridge.h>
+#include <Temboo.h>
+#include "TembooAccount.h"; 
+#define TEMBOO_ACCOUNT "iotrepeat"; 
+#define TEMBOO_APP_KEY_NAME "myFirstApp";
+#define TEMBOO_APP_KEY  "xG7CvCS2fQs6oy6VTXxwaobq8BMgdMki";
 const String GMAIL_USER_NAME = "iotrepeat@gmail.com";
 const String GMAIL_PASSWORD = "QWERTY123?";
 const String TO_EMAIL_ADDRESS = "iotrepeat@gmail.com";
 
-
+boolean success = false;
 // the setup routine runs once when you press reset:
 void setup() {
   // initialize serial communication at 9600 bits per second:
@@ -24,6 +30,8 @@ void loop() {
   Serial.println(soundSensorPin);
   delay(1);
    if (soundSensorPin > 300) {
+   
+    digitalWrite(ledPin, HIGH);
 
     Serial.println("Running SendAnEmail...");
   
@@ -41,16 +49,29 @@ void loop() {
   
     SendEmailChoreo.addInput("iotrepeat@gmail.com", GMAIL_USER_NAME);
     
-    SendEmailChoreo.addInput("Password", GMAIL_PASSWORD);
+    SendEmailChoreo.addInput("QWERTY123?", GMAIL_PASSWORD);
  
     SendEmailChoreo.addInput("iotrepeat@gmail.com", TO_EMAIL_ADDRESS);
   
-    SendEmailChoreo.addInput("Subject", "Tazz is on the couch!");
+    SendEmailChoreo.addInput("Subject", "Tazz is Barking");
   
-    SendEmailChoreo.addInput("MessageBody", "Doggy being bold");
+    SendEmailChoreo.addInput("MessageBody", "Someone nearby");
 
     unsigned int returnCode = SendEmailChoreo.run();
 
-    else (
+    if (returnCode == 0) {
+        Serial.println("Success! Email sent!");
+        success = true;
+    } 
+    else {
   
+      while (SendEmailChoreo.available()) {
+        char c = SendEmailChoreo.read();
+        Serial.print(c);
+      }
+    } 
+    SendEmailChoreo.close();
+
+    delay(60000);
+  }
 }
